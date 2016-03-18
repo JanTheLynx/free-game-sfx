61aee7bc1c4658395a05f3904110859426641dfe
saniv <snv985@gmail.com>
2016-03-18 20:46:32 +0300

    update

---
+++ sound/freedroid/Credits.lua
@@ -0,0 +1,160 @@
+---------------------------------------------------------------------
+-- This file is part of Freedroid
+--
+-- Freedroid is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- Freedroid is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Freedroid; see the file COPYING. If not, write to the
+-- Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+-- MA 02111-1307 USA
+----------------------------------------------------------------------
+
+title_screen{
+background = "credits.jpg",
+song = "HellFortressTwo.ogg",
+text = [[
+            FreedroidRPG 0.16
+
+PROGRAMMING:
+
+        Bob
+        Samuel Degrande
+        Mike Fleischmann
+        Scott Furry
+        Arthur Huillet
+        Jann Horn
+        Józef Kucia
+        Gregory Lozet
+        Michael Mendelson
+        Miles McCammon
+        Michael Parks
+        Samuel Pitoiset
+        v4hn
+        Xenux
+
+GAME CONTENT:
+
+        Amy de Buitléir
+        Infrared
+        Raymond Jennings
+        Jonatas L. Nogueira
+        Matthias Krüger
+        Miles McCammon
+
+GRAPHICS:
+
+        Infrared
+        Bastian Salmela
+
+MUSIC:
+
+        Nick Hagman
+
+TRANSLATIONS:
+
+        cs:
+          Jan Tojnar
+          Zdenek
+
+        de_DE:
+          JM Franz
+          Matthias Krüger
+          Link Mario
+
+        fr_FR:
+          Sebastian Adam
+          Alérion
+          Samuel Degrande
+          Nicolas Fargier
+          Benjamin Leduc
+          Duong Khang Nguyen
+          Xenux
+
+        pt_BR:
+          Anelise D. J.
+          Luiz Phelipe Ferreira da Silva
+          Jonatas L. Nogueira
+
+        sv_SV:
+          Alexander Forsberg
+
+MISC:
+
+        Eino Keskitalo
+        Duong Khang Nguyen
+
+
+
+            ALSO, IN PREVIOUS VERSIONS
+
+PROGRAMMING:
+
+        Maria Grazia Alastra
+        Pedro Arana
+        Catalin Badea
+        Pierre Bourdon
+        Bryan C
+        Simon Castellan
+        jcoral
+        Stefan Kangas
+        Pavaluca Matei
+        Michael Mendelson
+        Esa-Matti Mourujarvi
+        Ari Mustonen
+        Nicolas Pepin-Perreault
+        Quentin Pradet
+        Johannes Prix
+        Reinhard Prix
+        Alexander Solovets
+        Philippe Tetar
+
+GAME CONTENT:
+
+        Stefan Huszics
+        James
+        Sebastian Offermann
+        Kurtis Parramore
+        Niklas Spille
+        Starminn
+        Karol Swietlicki
+        rudi_s
+        Thor
+        JK Wood
+
+MUSIC:
+
+        Nick "Nario" Hagman
+        Arvid Picciani
+        "The Beginning"
+            by 4t thieves
+        "Daybreak"
+            by realsmokers
+        "Bleostrada"
+            by stud
+
+MISC:
+
+        Mbrola - text-to-speech
+        eSpeak - text-to-speech
+        Ryan 'simcop2387' Voots
+        Andrew A. Gill
+        Zombie Ryushu
+        Ted Cipicchio
+        The Doctor
+        Simon Newton
+        Clint Herron
+        Hike Danakian
+        Ian Griffiths
+        David Kremer
+        Armin Winterer
+]]
+}
+
